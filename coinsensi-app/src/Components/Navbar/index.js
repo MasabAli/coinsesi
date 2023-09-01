b998d61fe7f9e6f505f6d80c788d3c1266939ce1
Masab Ali <masabali143@gmail.com>
2023-09-02 00:17:38 +0500

    setting nav

--- coinsensi-app/src/Components/Navbar/index.js
+++ coinsensi-app/src/Components/Navbar/index.js
@@ -1,89 +1,111 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
+const Navbar = styled(AppBar)({
+  backgroundColor: "transparent",
+  boxShadow: "none",
+});
+
+const Logo = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+const MenuItems = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  marginLeft: "auto", // Push menu items to the right on large screens
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+  },
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
+const HamburgerMenu = styled("div")(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.down("sm")]: {
+    display: "block",
   },
 }));
 
 export default function SearchAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box container sx={{ flexGrow: 1 }}>
+      <Navbar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
+          <Logo>
+            <img src="/fav.webp" alt="logo" style={{ height: "70px" }} />
+            <Typography variant="h6" sx={{fontSize:"25px", fontFamily:"sans-serif", fontWeight:"bolder"}}>
+              <span style={{ color: "black" }}>Coin</span>
+              <span style={{ color: "black" }}>Sensi</span>
+            </Typography>
+          </Logo>
+          <MenuItems sx={{ marginRight: "50px" }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              Home
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              About
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              Services
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              Reviews
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              Contact
+            </Typography>
+          </MenuItems>
+          <HamburgerMenu>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ marginRight: 2, color: "black" }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </HamburgerMenu>
         </Toolbar>
-      </AppBar>
+      </Navbar>
     </Box>
   );
 }
