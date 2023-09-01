ba9f3e0bde70d428773beb94999e1052addeab69
Masab Ali <masabali143@gmail.com>
2023-09-01 22:32:50 +0500

    added nav

--- coinsensi-app/src/App.js
+++ coinsensi-app/src/App.js
@@ -1,9 +1,11 @@
-
+import SearchAppBar from './Components/Navbar/index.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+     
+    <SearchAppBar />
     </div>
   );
 }
